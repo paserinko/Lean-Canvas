7587eba1b326a28c870cd48cb85ec26010d19c2c
s-hasegawa <s-hasegawa@3sss.co.jp>
2021-11-09 21:27:25 +0900

    長谷川　レイアウト適当にやったで

--- sagawa/src/main/java/jp/co/sss/sagawa/Controller/IndexController.java
+++ sagawa/src/main/java/jp/co/sss/sagawa/Controller/IndexController.java
@@ -3,12 +3,17 @@ package jp.co.sss.sagawa.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller  
+@Controller
 public class IndexController {
-	@RequestMapping(path="/")
-	public String index() {
-		return"index";
+	@RequestMapping(path = "/")
+	public String top() {
+		return "top";
 	}
-	
-	//comennto!!!!!
+
+	@RequestMapping(path = "/login")
+	public String login() {
+		return "index";
+
+	}
+
 }
